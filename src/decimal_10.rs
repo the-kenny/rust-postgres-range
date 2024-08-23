50261bb636af0411ee066eb1a31863b8da1d1042
Rohit Singh <27754873+razein97@users.noreply.github.com>
2024-08-24 00:16:51 +0530

    Update decimal_10.rs

--- src/decimal_10.rs
+++ src/decimal_10.rs
@@ -1,6 +1,9 @@
 use crate::{BoundSided, Normalizable, RangeBound};
 use bigdecimal_04::BigDecimal;
 
+use crate::{BoundSided, Normalizable, RangeBound};
+use decimal_10::Decimal;
+
 impl Normalizable for Decimal {
     fn normalize<S>(bound: RangeBound<S, Decimal>) -> RangeBound<S, Decimal>
     where
